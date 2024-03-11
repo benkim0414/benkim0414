faa194a7b3a9f11cdf0a3055b81c505cd4679719
Gunwoo Ben Kim <benkim0414@gmail.com>
2024-03-11 14:36:45 +1100

    chore(lint-staged): add pre-commit hook to lint and format staged files

--- packages/k/src/k8s/client.spec.ts
+++ packages/k/src/k8s/client.spec.ts
@@ -5,13 +5,11 @@ import { createV1Pod } from './testutil';
 describe('AggregatedCoreV1Api', () => {
   describe('searchNamespacedPod', () => {
     it('should search the pod with the given name via the approximate string matching', async () => {
-      CoreV1Api.prototype.listNamespacedPod = jest
-        .fn()
-        .mockResolvedValue({
-          body: {
-            items: ['my-pod', 'your-pod', 'my-fancy-pod'].map(createV1Pod),
-          },
-        });
+      CoreV1Api.prototype.listNamespacedPod = jest.fn().mockResolvedValue({
+        body: {
+          items: ['my-pod', 'your-pod', 'my-fancy-pod'].map(createV1Pod),
+        },
+      });
       const client = new AggregatedCoreV1Api();
       expect(await client.searchNamespacedPod('my-pod', 'default')).toEqual(
         ['my-pod', 'my-fancy-pod'].map(createV1Pod)
