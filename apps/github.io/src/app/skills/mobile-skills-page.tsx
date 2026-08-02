8c556087b7c72b0b0a78cb104d553c7ce3e031eb
Gunwoo Ben Kim <benkim0414@gmail.com>
2026-08-02 14:43:00 +1000

    fix(github.io): add skill avatar size prop

--- apps/github.io/src/app/skills/mobile-skills-page.tsx
+++ apps/github.io/src/app/skills/mobile-skills-page.tsx
@@ -39,7 +39,7 @@ export interface MobileSkillsPageProps {
 function SkillCommandResult({ skill }: { skill: Skill }): ReactElement {
   return (
     <HStack gap={2} vAlign="center">
-      <SkillAvatar skill={skill} />
+      <SkillAvatar skill={skill} size="tiny" />
       <Text type="body">{skill.name}</Text>
     </HStack>
   );
