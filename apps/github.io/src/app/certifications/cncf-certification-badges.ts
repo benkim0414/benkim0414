fcc2b7a9c07a31b14d399bd747b6de78e3507d5f
Gunwoo Ben Kim <benkim0414@gmail.com>
2026-08-02 15:32:33 +1000

    feat(github.io): add CNCF certification badge assets

---
+++ apps/github.io/src/app/certifications/cncf-certification-badges.ts
@@ -0,0 +1,9 @@
+import ckaBadge from '../../assets/certifications/cncf/cka.png';
+import ckadBadge from '../../assets/certifications/cncf/ckad.png';
+import kcnaBadge from '../../assets/certifications/cncf/kcna.png';
+
+export const cncfCertificationBadges = {
+  KCNA: kcnaBadge,
+  CKAD: ckadBadge,
+  CKA: ckaBadge,
+} as const;
