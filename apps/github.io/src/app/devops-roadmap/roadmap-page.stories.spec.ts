c5cfff069f2ba012a5a2c8bca1b096b06682d00e
Gunwoo Ben Kim <benkim0414@gmail.com>
2026-08-14 10:31:18 +1000

    test(github.io): cover DevOps roadmap page story

---
+++ apps/github.io/src/app/devops-roadmap/roadmap-page.stories.spec.ts
@@ -0,0 +1,7 @@
+import meta from './roadmap-page.stories';
+
+describe('RoadmapPage stories', () => {
+  it('uses the Roadmap Page Storybook hierarchy', () => {
+    expect(meta.title).toBe('GitHub.io/DevOps Roadmap/Roadmap Page');
+  });
+});
